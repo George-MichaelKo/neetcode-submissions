db9a432b21fd3d87c67299cc077eb99b1602059d
George-Michael Kondoun <148492919+George-MichaelKo@users.noreply.github.com>
2026-08-19 17:53:34 -0500

    Add: longest-repeating-substring-with-replacement - submission-6

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        l = 0
+        have = 0 
+        ans = float('-inf')
+        freq = {}
+        for r in range(len(s)):
+            freq[s[r]] = freq.get(s[r], 0)+1
+
+            have = max(freq.values())
+            window_size = r-l+1
+            need = window_size - have
+
+            while need > k:
+                freq[s[l]] -= 1
+                l+=1
+                have = max(freq.values())
+                window_size = r-l+1
+                need = window_size - have
+            ans = max(ans, r-l+1)
+        return ans
+
+        
